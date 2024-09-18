a7ff253aa4514202a01f5a8e7f45a0108b99b5fc
Aleksandrs Vorobjovs <avorobjovs@gmail.com>
2024-09-18 21:07:57 +0300

    Update vite.config.ts (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/' // for a deployment to GitHub Pages
+  base: '/pet-project-frontend/' // for a deployment to GitHub Pages
 })
