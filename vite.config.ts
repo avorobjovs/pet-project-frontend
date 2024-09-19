f2a527e99cb5ba55280f2a55e7ce6cee6235b1b3
Aleksandrs Vorobjovs <avorobjovs@gmail.com>
2024-09-19 16:05:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { ConfigEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }: ConfigEnv) => {
+export default defineConfig(({ command, mode }: ConfigEnv) => {
   if (command === 'serve') {
     // dev specific config
     return {
