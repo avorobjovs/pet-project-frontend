27b8e1d3325262605d92d452b07e152a7614c5e1
Aleksandrs Vorobjovs <avorobjovs@gmail.com>
2024-09-19 16:14:32 +0300

    Update vite.config.ts (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// @ts-expect-error because
 export default defineConfig(({ command, mode }: ConfigEnv) => {
   if (command === 'serve') {
     // dev specific config
