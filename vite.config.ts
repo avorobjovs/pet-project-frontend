de512920de3cb8edf49d440680c887e938c84adb
Aleksandrs Vorobjovs <avorobjovs@gmail.com>
2024-09-19 16:28:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { ConfigEnv, defineConfig } from 'vite'
+//import { ConfigEnv, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
