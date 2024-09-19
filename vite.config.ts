d179f6251d0c47d105a7c46713115d22fe032850
Aleksandrs Vorobjovs <aleksandrs.vorobjovs@emergn.com>
2024-09-19 15:59:05 +0300

    Add Vite conditional configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE
+export default defineConfig(({ command }: ConfigEnv) => {
+  if (command === 'serve') {
+    // dev specific config
+    return {
+      plugins: [react()]
+    }
+  } else {
+    // command === 'build'
+    // build specific config
+    return {
+      plugins: [react()],
+      base: '/pet-project-frontend/'
+    }
   }
 })
