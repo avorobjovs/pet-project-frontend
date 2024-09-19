34cb63abc4d8983120f08c37ba98649ecdb67443
Aleksandrs Vorobjovs <aleksandrs.vorobjovs@emergn.com>
2024-09-19 17:23:22 +0300

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-//import { ConfigEnv, defineConfig } from 'vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-/*
-export default defineConfig(({ command, mode }: ConfigEnv) => {
-  if (command === 'build' || mode == 'production') {
-    return {
-      plugins: [react()],
-      base: '/pet-project-frontend/'
-    }
-  } else {
-    return {
-      plugins: [react()],
-      base: '/development-test/'
-    }
-  }
-})
-*/
-
 export default defineConfig({
   plugins: [react()],
-  base: '/pet-project-frontend/'
 })
